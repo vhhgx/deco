579a035bbfe92d9b06ca28a8b3ffb394960d0550
vhhgx <qieka_zhang@yeah.net>
2023-10-30 00:11:00 +0800

    feat 添加ico

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Deco",
-  description: "扬灵的装修记录",
+  title: 'Deco',
+  description: '扬灵的装修记录',
+  head: [['link', {rel: 'icon', href: '/favicon.ico'}]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
@@ -16,13 +17,13 @@ export default defineConfig({
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    // socialLinks: [
+    //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    // ]
+  },
 })
